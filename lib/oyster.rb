8b591add9a005065c4be0c0c4537e8225c703bc5
Student <student@Makerss-MacBook-Air-2.local>
2020-02-24 15:13:41 +0000

    05 oyster

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,9 +1,14 @@
-
+#In order to keep using public transport
+#As a customer
+#I want to add money to my card
 class OysterCard 
   DEFAULT_BALANCE = 0
   attr_accessor :balance
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+  end
+  def top_up(value)
+    @balance += value
   end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,20 @@
-require 'oyster_stories'
+require 'oyster'
 describe OysterCard do
   describe '#initialize' do
     it 'has a default of zero' do
       expect(subject.balance).to eq 0
     end
   end
+    describe '#top_up' do 
+      it 'top_up' do
+        oyster = OysterCard.new
+        oyster.top_up(10)
+       expect(oyster.balance).to eq(10)
+      end 
+    it 'adds amount to balance' do
+      oyster = OysterCard.new(5)
+      oyster.top_up(10)
+     expect(oyster.balance).to eq(15)
+    end
+    end
 end
