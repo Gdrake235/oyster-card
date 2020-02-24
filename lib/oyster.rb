e605510e2c10df3e5c57d14c5948aa7f08520058
Student <student@Makerss-MacBook-Air-2.local>
2020-02-24 16:07:32 +0000

    exercise 6

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,14 +1,22 @@
-#In order to keep using public transport
+#In order to protect my money from theft or loss
 #As a customer
-#I want to add money to my card
+#I want a maximum limit (of £90) on my card
 class OysterCard 
   DEFAULT_BALANCE = 0
-  attr_accessor :balance
+  attr_accessor :balance, :value
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
   def top_up(value)
+    @balance_with_top_up = @balance + value
+    fail 'exceeded balance of £90' if exceeded_limit?
     @balance += value
   end
-  
+  def exceeded_limit?
+    if @balance_with_top_up > 90
+      exceeded_limit = true
+    else
+      exceeded_limit = false
+    end
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,16 +5,23 @@ describe OysterCard do
       expect(subject.balance).to eq 0
     end
   end
-    describe '#top_up' do 
-      it 'top_up' do
-        oyster = OysterCard.new
-        oyster.top_up(10)
-       expect(oyster.balance).to eq(10)
-      end 
+  
+  describe '#top_up' do 
+    it 'top_up' do
+      oyster = OysterCard.new
+      oyster.top_up(10)
+      expect(oyster.balance).to eq(10)
+    end 
+    
     it 'adds amount to balance' do
       oyster = OysterCard.new(5)
       oyster.top_up(10)
-     expect(oyster.balance).to eq(15)
+      expect(oyster.balance).to eq(15)
     end
+
+    it 'has max balance £90' do
+      oyster = OysterCard.new(45)
+      expect{oyster.top_up(50)}.to raise_error 'exceeded balance of £90'
     end
+  end
 end
